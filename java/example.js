4b50da64beec1df179927b74d998f272a1906c62
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-01-29 18:37:35 +0100

    changed new code

--- java/example.js
+++ java/example.js
@@ -1,6 +1,5 @@
 const var1 = 'Hello my name is hassan. Nice to meet you guys!';
 const var2 = "I was born in ";
-const var3 = 2003;
 
 console.log(var1);
-console.log(var2, var3);
+console.log(var2, 2003);
