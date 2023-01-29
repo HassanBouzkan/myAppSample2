b932d4564fec37b22541cd60f40c3927288d747b
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-01-29 18:20:18 +0100

    create example.java

---
+++ java/example.java
@@ -0,0 +1,25 @@
+ 
+public class example {
+
+	public static void main(String[] args) {
+		
+	String name = "Ahmed";
+	
+	switch (name) {
+	case"Ahmed" : System.out.println("That's Ahmed");
+	break;
+		
+	case"Hassan" : System.out.println("That's Hassan");
+	break;
+	
+	case"Aymen" : System.out.println("That's Aymen");
+	break;
+	
+	case"Ikram" : System.out.println("That's Ikram");
+	break;
+	
+	default : System.out.println("Not within the list");
+	}
+	}
+
+}
